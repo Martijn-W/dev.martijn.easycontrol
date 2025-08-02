412d21f7a48218dee869e71cf90a27cfcc37c3bf
Martijn-W <martijnwoolschot@gmail.com>
2025-08-02 15:57:36 +0200

    Implemented syncing

--- drivers/thermostat-ct200/device.ts
+++ drivers/thermostat-ct200/device.ts
@@ -4,14 +4,18 @@ import DeviceData from './deviceData';
 
 module.exports = class extends Homey.Device {
     #client: Client = new Client();
+    #shouldSync: boolean = true;
+    #isSyncing: boolean = false;
+    #timeout: NodeJS.Timeout | null = null;
 
     async onInit() {
         const deviceData = this.getSetting('device') as DeviceData;
 
         await this.#client.connect(deviceData.serialNumber, deviceData.accessKey, deviceData.password);
-        await this.setInitialValues();
 
         this.log('EasyControl device has been initialized');
+
+        await this.sync();
     }
 
     async onAdded() {
@@ -23,12 +27,34 @@ module.exports = class extends Homey.Device {
     }
 
     async onDeleted() {
+        this.#shouldSync = false;
+        this.#timeout && clearTimeout(this.#timeout);
         this.#client.disconnect();
 
         this.log('EasyControl device has been deleted');
     }
 
-    private async setInitialValues() {
+    private async sync() {
+        if (!this.#shouldSync || this.#isSyncing)
+            return;
+
+        this.log('Syncing data...');
+
+        this.#isSyncing = true;
+
+        await this.setThermostatData();
+
+        this.#isSyncing = false;
+
+        this.log('Data synced');
+
+        this.#timeout = setTimeout(
+            async () => await this.sync(),
+            10000 //todo: get from settings
+        );
+    }
+
+    private async setThermostatData() {
         const zoneId = this.getSetting('zoneId') as number;
 
         const zoneTemperature = await this.#client.getZoneTemperature(zoneId);
@@ -37,25 +63,25 @@ module.exports = class extends Homey.Device {
         const systemPressure = await this.#client.getApplianceSystemPressure();
 
         if (zoneTemperature != null) {
-            console.log(`Current temperature: ${zoneTemperature.value}${zoneTemperature?.unitOfMeasure}`);
+            this.log(`Current temperature: ${zoneTemperature.value}${zoneTemperature?.unitOfMeasure}`);
 
             this.setCapabilityValue('measure_temperature', zoneTemperature.value).catch(this.error);
         }
 
         if (zoneTargetTemperature != null) {
-            console.log(`Current temperature: ${zoneTargetTemperature.value}${zoneTemperature?.unitOfMeasure}`);
+            this.log(`Current temperature: ${zoneTargetTemperature.value}${zoneTemperature?.unitOfMeasure}`);
 
             this.setCapabilityValue('target_temperature', zoneTargetTemperature.value).catch(this.error);
         }
 
         if (zoneHumidity != null) {
-            console.log(`Current humidity: ${zoneHumidity.value}${zoneHumidity?.unitOfMeasure}`);
+            this.log(`Current humidity: ${zoneHumidity.value}${zoneHumidity?.unitOfMeasure}`);
 
             this.setCapabilityValue('measure_humidity', zoneHumidity.value).catch(this.error);
         }
 
         if (systemPressure != null) {
-            console.log(`Current system pressure: ${systemPressure.value}${systemPressure?.unitOfMeasure}`);
+            this.log(`Current system pressure: ${systemPressure.value}${systemPressure?.unitOfMeasure}`);
 
             // Convert from bar to millibar
             const pressure = systemPressure.value * 1000;
